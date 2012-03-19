83f858651e49e62e7fba9a1839c3ab323f77b7f7
Hannu Leinonen <hleinone@gmail.com>
2012-03-20 00:57:21 +0200

    Changed H5BP mobile helper.js to use xui instead of jQuery.

--- js/helper.js
+++ js/helper.js
@@ -282,7 +282,7 @@ MBP.enableActive = function () {
 // Prevent iOS from zooming onfocus
 // http://nerd.vasilis.nl/prevent-ios-from-zooming-onfocus/
 
-MBP.viewportMeta = $('meta[name="viewport"]');
+MBP.viewportMeta = x$('meta[name=viewport]');
 MBP.preventZoom = function () {
   $('input, select, textarea').bind('focus blur', function(event) {
     MBP.viewportMeta.attr('content', 'width=device-width,initial-scale=1,maximum-scale=' + (event.type == 'blur' ? 10 : 1));
